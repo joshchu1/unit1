9560fe717751376a8747d8d22155be440ea293a5
joshchu1 <31485809+joshchu1@users.noreply.github.com>
2017-09-05 21:28:57 -0400

    Updated from Brython Server: 9/5/2017 9:28:56 PM

--- km2miles.py
+++ km2miles.py
@@ -6,7 +6,7 @@
 kilometers = float(input("number of kilometers"))
 print (kilometers)
 #conversion factor
-conv_fac=0.62
+conv_fac=0.6217
 #calculate miles
 miles=kilometers*conv_fac
-print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles)) 
+print(miles)
